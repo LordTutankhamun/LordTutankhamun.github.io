d3b75f78bd01e2bb1a7ee750d76ac574a6aa344f
LordTutankhamun <93034111+LordTutankhamun@users.noreply.github.com>
2021-10-23 15:42:48 +0100

    Add files via upload
    The BUTTON

--- website.js
+++ website.js
@@ -18,3 +18,22 @@ function openTab(evt, tabId) {
   document.getElementById(tabId).style.display = "block";
   evt.currentTarget.className += " active";
 }
+function searchAssets() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("h2")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
